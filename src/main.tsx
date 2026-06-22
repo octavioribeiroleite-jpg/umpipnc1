312148ef636f33ac59a822e1403bcc4bc1b5fbd5
octavioribeiroleite-jpg <octavioribeiroleite@gmail.com>
2026-06-22 14:25:52 -0300

    Ativa melhorias visuais da tela de entrada

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./auth-readability.css";
 import { registerServiceWorker } from "./lib/registerSW";
 
 createRoot(document.getElementById("root")!).render(<App />);
