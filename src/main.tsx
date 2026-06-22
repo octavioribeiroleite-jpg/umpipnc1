3a8ef47caee63dbfefb192d6ee5ab0bbb72edc60
octavioribeiroleite-jpg <octavioribeiroleite@gmail.com>
2026-06-22 15:57:30 -0300

    Ativa novo visual da confirmação de acesso

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import SocietyScreenEnhancer from "./components/auth/SocietyScreenEnhancer";
+import IdentityConfirmationEnhancer from "./components/auth/IdentityConfirmationEnhancer";
 import "./index.css";
 import "./auth-readability.css";
 import "./society-selector.css";
+import "./identity-confirmation.css";
 import { registerServiceWorker } from "./lib/registerSW";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <SocietyScreenEnhancer />
+    <IdentityConfirmationEnhancer />
   </>,
 );
 
