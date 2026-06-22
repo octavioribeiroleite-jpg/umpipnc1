d85d129ffdb36c8ed67d5136e71e9063374e4f6f
octavioribeiroleite-jpg <octavioribeiroleite@gmail.com>
2026-06-22 14:38:37 -0300

    Ativa novo visual da seleção de sociedades

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import SocietyScreenEnhancer from "./components/auth/SocietyScreenEnhancer";
 import "./index.css";
 import "./auth-readability.css";
+import "./society-selector.css";
 import { registerServiceWorker } from "./lib/registerSW";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <SocietyScreenEnhancer />
+  </>,
+);
 
 registerServiceWorker();
